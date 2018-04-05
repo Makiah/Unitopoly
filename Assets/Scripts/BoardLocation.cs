ed726ee3cbbab004e153837e0638d9ae8fbe8b77
makiahtbennett@gmail.com <makiahtbennett@gmail.com>
2018-04-05 19:12:48 -0400

    Board has been set up, and skybox added

---
+++ Assets/Scripts/BoardLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BoardLocation : MonoBehaviour
+{
+    private BoardLocation previousSpace, nextSpace;
+    
+    private void Start()
+    {
+        int currentSpace = Int32.Parse(gameObject.name);
+        
+        nextSpace = currentSpace < 39 ? 
+            gameObject.transform.parent.Find((currentSpace + 1).ToString()).GetComponent<BoardLocation>() : 
+            gameObject.transform.parent.Find("0").GetComponent<BoardLocation>();
+        
+        previousSpace = currentSpace > 0 ? 
+            gameObject.transform.parent.Find((currentSpace - 1).ToString()).GetComponent<BoardLocation>() : 
+            gameObject.transform.parent.Find("39").GetComponent<BoardLocation>();
+    }
+}
